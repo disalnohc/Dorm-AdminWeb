d7d08af2c50505745d2604c6973b54f30c0ffe65
disalnohc <toeibew00@gmail.com>
2023-09-06 22:23:41 +0700

    update fetch data room
    - insert data room from firebase to table
    
    * no bug

--- src/pages/room/Room.js
+++ src/pages/room/Room.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./room.css";
 import DataTableAC from "../../components/dataTableAC/DataTableAC";
 import Add from "../../components/add/Add";
-import { electricity  } from "../../data.ts";
+import { firestore } from "../../firebase";
 
 const columns = [
-  { field: "id", headerName: "ID", width: 250 },
+  { field: "id", headerName: "ลำดับ", width: 100 }, // เปลี่ยนชื่อเป็น "ลำดับ"
   {
     field: "roomNumber",
     type: "string",
@@ -21,17 +21,40 @@ const columns = [
 ];
 
 const Room = () => {
-    const [open, setOpen] = useState(false);
-  
-    return (
-      <div className="products">
-        <div className="info">
-          <h1>ผังห้องพัก</h1>
-        </div>
-        <DataTableAC slug="products" columns={columns} rows={electricity} />
-        {open && <Add slug="product" columns={columns} setOpen={setOpen} />}
+  const [open, setOpen] = useState(false);
+  const [roomData, setRoomData] = useState([]); // เก็บข้อมูลจาก Firestore
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const collRef = firestore
+          .collection("Apartment")
+          .doc("Room")
+          .collection("RoomData");
+        const querySnapshot = await collRef.get();
+        const data = querySnapshot.docs.map((doc, index) => ({
+          id: index + 1, // เปลี่ยน id เป็นลำดับข้อมูล
+          roomNumber: doc.id,
+          status: doc.data().status,
+        }));
+        setRoomData(data);
+      } catch (error) {
+        console.log("เกิดข้อผิดพลาดในการดึงข้อมูล : ", error);
+      }
+    };
+
+    fetchData();
+  }, []); // เรียก fetchData() เมื่อคอมโพเนนต์ถูกโหลดเสร็จสิ้นเพียงครั้งเดียว
+
+  return (
+    <div className="products">
+      <div className="info">
+        <h1>ผังห้องพัก</h1>
       </div>
-    );
-  };
-  
-  export default Room;
+      <DataTableAC slug="products" columns={columns} rows={roomData} />
+      {open && <Add slug="product" columns={columns} setOpen={setOpen} />}
+    </div>
+  );
+};
+
+export default Room;
