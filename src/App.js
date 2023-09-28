a91ac7dc2771293e89c8a52f9ceff59b1def12e5
disalnohc <toeibew00@gmail.com>
2023-09-28 22:25:28 +0700

    fix not access user role
    - have bug when log out and log in new role

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState , useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import LoginForm from './pages/LoginForm';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import HeaderBar from './layout/HeaderBar';
@@ -23,87 +23,94 @@ import { auth } from './firebase';
 import { firestore } from './firebase';
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [isAdmin , setIsAdmin] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
     const isLogin = auth.onAuthStateChanged((authUser) => {
-      if(authUser) {
+      if (authUser) {
         setIsAuthenticated(true);
         const user = authUser.uid;
+        console.log(user)
+        const docRef = firestore.collection('profiles').doc(user);
 
-        const userData = firestore.collection('profiles').doc(user).get();
-
-      if (userData.exists) {
-        const userRole = userData.data().role;
-        if (userRole === 'admin') {
-          setIsAdmin(true);
-        } else {
-          setIsAdmin(false);
-        }
-      } else {
-        console.log('ไม่พบข้อมูลผู้ใช้');
-      }
-
+        docRef.get()
+          .then((doc) => {
+            if (doc.exists) {
+              const userData = doc.data();
+              const role = userData.role;
+              console.log("Role:", role);
+              if (role === 'admin') {
+                setIsAdmin(true);
+              } else {
+                setIsAdmin(false);
+              }
+            } else {
+              console.log("ไม่พบเอกสารสำหรับผู้ใช้นี้");
+            }
+          })
+          .catch((error) => {
+            console.log("เกิดข้อผิดพลาดในการดึงข้อมูล:", error);
+          });
       }
     });
     return () => {
       isLogin();
     };
-  },[]);
+  }, []);
 
   function admin_menu() {
-    return(
+    return (
       <>
-            <SideBar />
-            <main className="content">
-              <HeaderBar setIsAuthenticated={setIsAuthenticated}/>
-              <div className="content_body">
-                <Box m="20px">
-                  <Routes>
-                    <Route path="/" element={<Navigate to="/admin/dashboard" />} />
-                    <Route path="/admin/dashboard" element={<Dashboard />} />
-                    <Route path="/admin/calendar" element={<Calender />} />
-                    <Route path="/admin/news" element={<News />} />
-                    <Route path="/admin/clean" element={<Clean />} />
-                    <Route path="/admin/detail" element={<Detail />} />
-                    <Route path="/admin/notifybill" element={<Notifybill />} />
-                    <Route path="/admin/paybill" element={<Paybill />} />
-                    <Route path="/admin/personnel" element={<Personnel />} />
-                    <Route path="/admin/repair" element={<Repair />} />
-                    <Route path="/admin/room" element={<Room />} />
-                    <Route path="/admin/water" element={<Water />} />
-                    <Route path="/admin/electricity" element={<Electricity />} />
-                    <Route path="/admin/security" element={<Security />} />
-                    <Route path="/admin/profile" element={<Profile />} />
-                  </Routes>
-                </Box>
-              </div>
-            </main>
-          </>
+        <SideBar />
+        <main className="content">
+          <HeaderBar setIsAuthenticated={setIsAuthenticated} />
+          <div className="content_body">
+            <Box m="20px">
+              <Routes>
+                <Route path="/" element={<Navigate to="/admin/dashboard" />} />
+                <Route path="/admin/dashboard" element={<Dashboard />} />
+                <Route path="/admin/calendar" element={<Calender />} />
+                <Route path="/admin/news" element={<News />} />
+                <Route path="/admin/clean" element={<Clean />} />
+                <Route path="/admin/detail" element={<Detail />} />
+                <Route path="/admin/notifybill" element={<Notifybill />} />
+                <Route path="/admin/paybill" element={<Paybill />} />
+                <Route path="/admin/personnel" element={<Personnel />} />
+                <Route path="/admin/repair" element={<Repair />} />
+                <Route path="/admin/room" element={<Room />} />
+                <Route path="/admin/water" element={<Water />} />
+                <Route path="/admin/electricity" element={<Electricity />} />
+                <Route path="/admin/security" element={<Security />} />
+                <Route path="/admin/profile" element={<Profile />} />
+              </Routes>
+            </Box>
+          </div>
+        </main>
+      </>
     )
   }
 
   function user_menu() {
-    return(
+    return (
       <>
-            <SideBar />
-            <main className="content">
-              <HeaderBar setIsAuthenticated={setIsAuthenticated}/>
-              <div className="content_body">
-                <Box m="20px">
-                  <Routes>
-                    <Route path="/" element={<Navigate to="/user/profile" />} /> {/* ใส่ Route User ตรงนี้ */}
-                    <Route path="/user/profile" element={<Profile />} />
-                  </Routes>
-                </Box>
-              </div>
-            </main>
-          </>
+        <SideBar />
+        <main className="content">
+          <HeaderBar setIsAuthenticated={setIsAuthenticated} />
+          <div className="content_body">
+            <Box m="20px">
+              <Routes>
+                <Route path="/" element={<Navigate to="/user/profile" />} /> {/* ใส่ Route User ตรงนี้ */}
+                <Route path="/user/profile" element={<Profile />} />
+              </Routes>
+            </Box>
+          </div>
+        </main>
+      </>
     )
   }
 
   function login_menu() {
-    return(
+    return (
       <Routes>
         <Route path="/" element={<LoginForm onLogin={() => setIsAuthenticated(true)} />} />
       </Routes>
